43228c87aa2dc275dd5a3d5b31a5e999a0a95e01
David Bruant <bruant@enseirb.fr>
2010-04-23 17:34:35 -0700

    reduceRight map filter

--- ecma5array.js
+++ ecma5array.js
@@ -1,4 +1,4 @@
-/*
+﻿/*
 Copyright (c) 2010, Toby Ho
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -27,52 +27,6 @@ THE SOFTWARE.
 
     var undefined; // cache the undefined value for quick access and to prevent uncontrolled undefined overrides
     
-    (function(){
-        // Object.getPrototypeOf implementation by John Resig. (http://ejohn.org/blog/objectgetprototypeof/)
-        if ( typeof Object.getPrototypeOf !== "function" ) {
-          if ( typeof "test".__proto__ === "object" ) {
-            Object.getPrototypeOf = function(object){
-              return object.__proto__;
-            };
-          } else {
-            Object.getPrototypeOf = function(object){
-              // May break if the constructor has been tampered with
-              return object.constructor.prototype;
-            };
-          }
-        }
-    })();
-    
-    function isNotUndefined(v){
-        return v !== undefined;
-    }
-    
-    var isPropertyDefined = typeof Object.getOwnPropertyDescriptor === "function" ?
-                                function isPropertyDefined(O, name){
-                                    if(typeof O !== "object")
-                                        throw new TypeError("First argument of isPropertyDefined is not an object");
-                                    
-                                    var currentObject = O;
-                                    
-                                    while(currentObject !== null){ // The prototype chain ends with a null
-                                        if(Object.getOwnPropertyDescriptor(O, name) !== undefined)
-                                            return true; // A property with the right name has been found in the prototype chain
-                                        
-                                        currentObject = Object.getPrototypeOf(currentObject);
-                                    }
-                                    
-                                    return false;
-                                    
-                                }:
-                                function isPropertyDefined(O, name){
-                                    if(typeof O !== "object")
-                                        throw new TypeError("First argument of isPropertyDefined is not an object");
-                                    
-                                    return isNotUndefined(O[name]); // No way to differentiate a property set to undefined or unset
-                                }; 
-    
-    
-    
     function arrayMethods(methods){
       var arrProto = Array.prototype;
       for (var name in methods){
@@ -81,79 +35,116 @@ THE SOFTWARE.
       }
     }
 
+    
     arrayMethods({
       forEach: function forEach(iter){
-        var i,l;
-        var context = arguments[1];
+        var i, l, context = arguments[1];
 
-        if(typeof iter !== "function"){ // ES5 : "If IsCallable(callbackfn) is false, throw a TypeError exception."
+        if(typeof iter !== "function") // ES5 : "If IsCallable(callbackfn) is false, throw a TypeError exception."
             throw new TypeError("Array.prototype.forEach method. First argument is not callable.");
-        }
         
-        for (i = 0, l = this.length ; i < l; i++){
-            if(isPropertyDefined(this, i.toString()))
+        for (i = 0, l = this.length ; i < l; i++){ // a for-in would not guarantee the elements to be traversed in the right ordre (ES5 12.6.4)
+            if(i in this)
                 iter.call(context, this[i], i, this);
         }
       }
       ,
       reduce: function reduce(accumlator){
-        var curr;
-        var arrayFirstDefinedIndex;
-        var targetArray;
+        var i, l = this.length, curr;
         
-        var i;
-        var l = this.length;
-        
-        if(typeof accumlator !== "function"){ // ES5 : "If IsCallable(callbackfn) is false, throw a TypeError exception."
+        if(typeof accumlator !== "function") // ES5 : "If IsCallable(callbackfn) is false, throw a TypeError exception."
             throw new TypeError("Array.prototype.reduce method. First argument is not callable");
-        }
 
-        if((l == 0 || l === null) && (arguments.length <= 1)){ // == on purpose to test 0 and false.
+        if((l == 0 || l === null) && (arguments.length <= 1))// == on purpose to test 0 and false.
             throw new TypeError("Array.prototype.reduce method. length is 0 and no second argument");
-        }
         
         if(arguments.length <= 1){
-        
-            for(i=0 ; i<l && !isPropertyDefined(this, i.toString()) ; i++){
-            }
+            for(i=0 ; i<l && !(i in this) ; i++){} // find the first defined element in the array if there is one
             
-            if(i===l){ // empty array
+            if(i>=l) // empty array
                 throw new TypeError("Array.prototype.reduce method. Empty array and no second argument");
-            }
             
-            curr = this[i++];
+            curr = this[i++]; // Increase i to start searching the secondly defined element in the array
         }
         else{
             curr = arguments[1];
         }
         
         for(i = i || 0 ; i < l ; i++){
-            if(isPropertyDefined(this, i.toString()))
+            if(i in this)
                 curr = accumlator.call(undefined, curr, this[i], i, this);
         }
         
         return curr;
       }
       ,
-      reduceRight: function(accumlator, initVal){
-        var curr = initVal;
-        for (var i = this.length - 1; i >= 0; i--){
-          var item = this[i];
-          curr = accumlator(curr, item, i, this);
+      reduceRight: function reduceRight(accumlator){
+        var i, l = this.length, curr;
+        
+        if(typeof accumlator !== "function") // ES5 : "If IsCallable(callbackfn) is false, throw a TypeError exception."
+            throw new TypeError("Array.prototype.reduce method. First argument is not callable");
+
+        if((l == 0 || l === null) && (arguments.length <= 1)) // == on purpose to test 0 and false.
+            throw new TypeError("Array.prototype.reduce method. length is 0 and no second argument");
+        
+        if(arguments.length <= 1){
+            for(i=l-1 ; i>=0 && !(i in this) ; i--){} // find the last defined element in the array if there is one
+            
+            if(i<0) // empty array
+                throw new TypeError("Array.prototype.reduce method. Empty array and no second argument");
+            
+            curr = this[i--]; // Decrease i to start searching the secondly defined element in the array in reverse order
         }
+        else{
+            curr = arguments[1];
+        }
+        
+        for(i = i || l-1 ; i >=0 ; i--){
+            if(i in this)
+                curr = accumlator.call(undefined, curr, this[i], i, this);
+        }
+        
         return curr;
       }
       ,
-      map: function(func, context){
-        return this.reduce(function(curr, item, i, arr){
-          return curr.concat([func.call(context, item, i, arr)])
-        }, []);
+      map: function map(func){
+        var ret;
+        var i;
+        var context = arguments[1];
+        var l = this.length;
+    
+        if(typeof func !== "function") // ES5 : "If IsCallable(callbackfn) is false, throw a TypeError exception."
+            throw new TypeError("Array.prototype.map method. First argument is not callable");
+  
+        ret = new Array(this.length);
+  
+        this.forEach(function(e,i,a){
+                        ret[i] = func.call(context, e, i, a);
+                     });
+        
+        return ret;
       }
       ,
-      filter: function(accept, context){
+      filter: function filter(accept){
+        var context = arguments[1];
+        var ret;
+        
+        if(typeof accept !== "function") // ES5 : "If IsCallable(callbackfn) is false, throw a TypeError exception."
+            throw new TypeError("Array.prototype.filter method. First argument is not callable");
+
+        ret = new Array();
+        
+        this.forEach(function(e, i, a){
+            if(accept.call(context, e, i, a) === true)
+                ret.push(e);
+        });
+            
+        return ret;/* 
+            
+        
         return this.reduce(function(curr, item, i, arr){
           return accept.call(context, item, i, arr) ? curr.concat(item) : curr;
-        }, []);
+        }, []); */
       }
       ,
       every: function(predicate, context){
